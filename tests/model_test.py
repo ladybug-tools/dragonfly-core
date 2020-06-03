83b12c81949e6036c25d1ac106b3a2e31751f228
Chris <chris@ladybug.tools>
2020-06-02 22:47:43 -0400

    feat(building): Add perimeter_offset as an option on classmethods
    This commit adds an input argument for core/perimeter offset to the two classmethods that can be used to create Buildings from footprint
    
    Current known limitations:
    1) Any geometry with a hole is currently ignored and will just result in a single Room2D for the story.
    2) The vast majority of geometries I tried from Rhino worked well but one particularly spiky one resulted in Rhino completely crashing. So I plan to turn it into a test such that @saeranv can hopefully catch this exception and avoid Rhino crashing.

--- tests/model_test.py
+++ tests/model_test.py
@@ -620,7 +620,7 @@ def test_to_geojson():
 def test_writer():
     """Test the Model writer object."""
     pts = (Point3D(50, 50, 3), Point3D(60, 50, 3), Point3D(60, 60, 3), Point3D(50, 60, 3))
-    bldg = Building.from_footprint('TestBldg', [Face3D(pts)], [5, 4, 3, 3], 0.01)
+    bldg = Building.from_footprint('TestBldg', [Face3D(pts)], [5, 4, 3, 3], tolerance=0.01)
     model = Model('TestModel', [bldg])
 
     writers = [mod for mod in dir(model.to) if not mod.startswith('_')]
