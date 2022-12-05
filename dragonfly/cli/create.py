aec735d46c85bd62f52d071fcdc0f4aed1ae3bc2
Chris Mackey <chris@ladybug.tools>
2022-12-05 18:20:19 -0500

    docs(model): Add commands to merge models

---
+++ dragonfly/cli/create.py
@@ -0,0 +1,49 @@
+"""dragonfly model creation commands."""
+import click
+import sys
+import logging
+import json
+
+from dragonfly.model import Model
+
+_logger = logging.getLogger(__name__)
+
+
+@click.group(help='Commands for creating Dragonfly models.')
+def create():
+    pass
+
+
+@create.command('merge-models')
+@click.argument('base-model', type=click.Path(
+    exists=True, file_okay=True, dir_okay=False, resolve_path=True))
+@click.option(
+    '--other-model', '-m', help='The other Model to be merged into the base model.',
+    type=click.File('w'), multiple=True)
+@click.option(
+    '--output-file', '-f', help='Optional file to output the Model JSON string'
+    ' with solved adjacency. By default it will be printed out to stdout',
+    type=click.File('w'), default='-')
+def convert_units(base_model, other_model, output_file):
+    """Create a Dragonfly Model by merging multiple models together.
+
+    \b
+    Args:
+        base_model: Full path to a Dragonfly Model JSON or Pkl file that serves
+            as the base into which the other model(s) will be merged. This model
+            determines the units and tolerance of the output model.
+    """
+    try:
+        # serialize the Model and convert the units
+        parsed_model = Model.from_file(base_model)
+        other_models = [Model.from_file(m) for m in other_model]
+        for o_model in other_models:
+            parsed_model.add_model(o_model)
+
+        # write the new model out to the file or stdout
+        output_file.write(json.dumps(parsed_model.to_dict()))
+    except Exception as e:
+        _logger.exception('Model merging failed.\n{}'.format(e))
+        sys.exit(1)
+    else:
+        sys.exit(0)
--- dragonfly/cli/translate.py
+++ dragonfly/cli/translate.py
@@ -10,6 +10,7 @@ from ladybug.location import Location
 from ladybug.futil import preparedir
 from honeybee.units import parse_distance_string, UNITS_TOLERANCES
 from honeybee.config import folders as hb_folders
+from honeybee.model import Model as HBModel
 
 from dragonfly.model import Model
 from dragonfly.windowparameter import SimpleWindowRatio
@@ -114,6 +115,81 @@ def model_to_honeybee(model_json, obj_per_model, multiplier, no_plenum, no_cap,
         log_file.write(json.dumps(hb_jsons, indent=4))
     except Exception as e:
         _logger.exception('Model translation failed.\n{}'.format(e))
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+
+@translate.command('merge-models-to-honeybee')
+@click.argument('base-model', type=click.Path(
+    exists=True, file_okay=True, dir_okay=False, resolve_path=True))
+@click.option(
+    '--dragonfly-model', '-d', help='Other Dragonfly Model to be merged into '
+    'the base model.', type=click.File('w'), multiple=True)
+@click.option(
+    '--honeybee-model', '-h', help='Other Honeybee Model to be merged into '
+    'the base model.', type=click.File('w'), multiple=True)
+@click.option(
+    '--multiplier/--full-geometry', ' /-fg', help='Flag to note if the '
+    'multipliers on each Building story should be passed along to the '
+    'generated Honeybee Room objects or if full geometry objects should be '
+    'written for each story in the building.', default=True, show_default=True)
+@click.option(
+    '--no-plenum/--plenum', ' /-p', help='Flag to indicate whether '
+    'ceiling/floor plenums should be auto-generated for the Rooms.',
+    default=True, show_default=True)
+@click.option(
+    '--no-ceil-adjacency/--ceil-adjacency', ' /-a', help='Flag to indicate '
+    'whether adjacencies should be solved between interior stories when '
+    'Room2Ds perfectly match one another in their floor plate. This ensures '
+    'that Surface boundary conditions are used instead of Adiabatic ones. '
+    'Note that this input has no effect when the object-per-model is Story.',
+    default=True, show_default=True)
+@click.option(
+    '--enforce-adj-check/--bypass-adj-check', ' /-bc', help='Flag to note '
+    'whether an exception should be raised if an adjacency between two '
+    'Room2Ds is invalid or if the check should be bypassed and the invalid '
+    'Surface boundary condition should be replaced with an Outdoor boundary '
+    'condition. If bypass is selected, any Walls containing WindowParameters '
+    'and an illegal boundary condition will also be replaced with an '
+    'Outdoor boundary condition.', default=True, show_default=True)
+@click.option(
+    '--output-file', '-f', help='Optional file to output the Honeybee Model JSON string'
+    ' with solved adjacency. By default it will be printed out to stdout',
+    type=click.File('w'), default='-')
+def convert_units(base_model, dragonfly_model, honeybee_model, multiplier,
+                  no_plenum, no_ceil_adjacency, enforce_adj_check, output_file):
+    """Merge multiple Dragonfly and/or Honeybee Models into a single Honeybee Model.
+
+    \b
+    Args:
+        base_model: Full path to a Dragonfly Model JSON or Pkl file that serves
+            as the base into which the other model(s) will be merged.
+    """
+    try:
+        # serialize the Model and convert the units
+        parsed_model = Model.from_file(base_model)
+        other_models = [Model.from_file(m) for m in dragonfly_model]
+        for o_model in other_models:
+            parsed_model.add_model(o_model)
+        
+        # convert the dragonfly Model to Honeybee
+        hb_model = add_plenum = not no_plenum
+        ceil_adjacency = not no_ceil_adjacency
+        hb_model = parsed_model.to_honeybee(
+            object_per_model='District', use_multiplier=multiplier,
+            add_plenum=add_plenum, solve_ceiling_adjacencies=ceil_adjacency,
+            enforce_adj=enforce_adj_check)
+
+        # merge the honeybee models
+        other_models = [HBModel.from_file(m) for m in honeybee_model]
+        for o_model in other_models:
+            hb_model.add_model(o_model)
+
+        # write the new model out to the file or stdout
+        output_file.write(json.dumps(hb_model.to_dict()))
+    except Exception as e:
+        _logger.exception('Model merging failed.\n{}'.format(e))
         sys.exit(1)
     else:
         sys.exit(0)
