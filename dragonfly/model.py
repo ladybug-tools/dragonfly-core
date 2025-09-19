0e24b5058887d17cc71a3f6e30f03c3e272e2104
Chris Mackey <chris@ladybug.tools>
2025-09-19 11:49:05 -0700

    fix(model): Ensure serialization failure in validation does not error

--- dragonfly/model.py
+++ dragonfly/model.py
@@ -2185,22 +2185,21 @@ class Model(_BaseGeometry):
             report = 'Input Model for validation is not a Model object, ' \
                 'file path to a Model or a Model DFJSON string.'
 
-        # get the function to call to do checks
-        if '.' in check_function:  # nested attribute
-            attributes = check_function.split('.')  # get all the sub-attributes
-            check_func = model
-            for attribute in attributes:
-                if check_func is None:
-                    continue
-                check_func = getattr(check_func, attribute, None)
-        else:
-            check_func = getattr(model, check_function, None)
-        assert check_func is not None, \
-            'Dragonfly Model class has no method {}'.format(check_function)
-
-        # process the arguments and options
-        args = [] if check_args is None else [] + list(check_args)
-        kwargs = {'raise_exception': False}
+        if report == '':  # get the function to call to do checks
+            if '.' in check_function:  # nested attribute
+                attributes = check_function.split('.')  # get all the sub-attributes
+                check_func = model
+                for attribute in attributes:
+                    if check_func is None:
+                        continue
+                    check_func = getattr(check_func, attribute, None)
+            else:
+                check_func = getattr(model, check_function, None)
+            assert check_func is not None, \
+                'Dragonfly Model class has no method {}'.format(check_function)
+            # process the arguments and options
+            args = [] if check_args is None else [] + list(check_args)
+            kwargs = {'raise_exception': False}
 
         # create the report
         if not json_output:  # create a plain text report
@@ -2210,8 +2209,9 @@ class Model(_BaseGeometry):
             ver_msg = 'Validating Model using dragonfly-core=={} and ' \
                 'dragonfly-schema=={}'.format(c_ver, s_ver)
             # run the check function
-            kwargs['detailed'] = False
-            report = check_func(*args, **kwargs)
+            if report == '':
+                kwargs['detailed'] = False
+                report = check_func(*args, **kwargs)
             # format the results of the check
             if report == '':
                 full_msg = ver_msg + '\nCongratulations! Your Model is valid!'
